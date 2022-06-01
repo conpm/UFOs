af0bf9d65ddb0526434dc8e2d7af76d076344409
Conor McGrane <100978920+conpm@users.noreply.github.com>
2022-06-01 10:51:19 -0600

    continued working on app.js file

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,9 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
 function buildTable(data){
 	// Clear out the table by building it as empty string.
 	tbody.html("");
@@ -14,4 +20,29 @@ function buildTable(data){
 			cell.text(val);
 		});
 	});
-}
+};
+
+function handleClick(){
+
+	// Grab the datetime value from the filter
+	let date = d3.select("#datetime").property("value");
+	let filteredData = tableData;
+
+	// Check to see if a date was entered and filter the data using that date
+	if (date){
+
+		// Apply `filter` to the table date to only keep the rows where `datetime`
+		// value matches the filter value
+		filteredData = filteredData.filter(row => row.datetime === date);
+	};
+
+	// Rebuild the table using the filtered data
+	// @NOTE: If no date was entered, then filteredData will just be the original tableData.
+	buildTable(filteredData);
+};
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
